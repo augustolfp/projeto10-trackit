0c2f89fde2372f2554236d6f2fc33cf6e9cf9e9d
augustolfp <augustolfp@gmail.com>
2022-05-25 20:20:58 -0300

    Campos da tela de Login funcionais

--- src/components/LoginScreen/LoginScreen.js
+++ src/components/LoginScreen/LoginScreen.js
@@ -1,13 +1,36 @@
 import logo from "../../assets/images/logoBig.svg";
+import React from "react";
+import styledComponents from "styled-components";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+
 export default function LoginScreen() {
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+
+    function handleLogin(event) {
+        event.preventDefault();
+        const body = {
+            email,
+            password
+        }
+
+        const loginRequest = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login", body)
+        loginRequest.then("funcionou");
+        loginRequest.catch("deu pau")
+
+    }
+
     return(
         <>
         <img src={logo} />
-        <form>
-            <input type="email" placeholder="email" />
-            <input type="password" placeholder="senha" />
+        <form onSubmit={handleLogin}>
+            <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="email" required />
+            <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="senha" required />
             <button type="submit">Entrar</button>
         </form>
+        <Link to="/cadastro">Não tem uma conta? Cadastre-se!</Link>
         </>
     );
 }
--- src/components/RegisterScreen/RegisterScreen.js
+++ src/components/RegisterScreen/RegisterScreen.js
@@ -1,5 +1,22 @@
+import logo from "../../assets/images/logoBig.svg";
+import React from "react";
 export default function RegisterScreen() {
+    const [form, setForm] = React.useState({
+        email: "",
+        password: "",
+        name: "",
+        image: ""
+    })
+
     return(
-        <div>TELA DE CADASTRO</div>
+        <>
+        <img src={logo} />
+        <form onSubmit={handleLogin}>
+            <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="email" required />
+            <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="senha" required />
+            <button type="submit">Cadastrar</button>
+        </form>
+        <Link to="/">Já tem uma conta? Faça login!</Link>
+        </>
     );
 }
