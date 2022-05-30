f3c6572536fe619d24fce60a8267be80e782150a
augustolfp <augustolfp@gmail.com>
2022-05-30 17:33:54 -0300

    Implementação do layout da página /historico

--- src/components/HistoryScreen/HistoryScreen.js
+++ src/components/HistoryScreen/HistoryScreen.js
@@ -1,5 +1,37 @@
+import TopBar from "../TopBar/TopBar";
+import BottomBar from "../BottomBar/BottomBar";
+import styled from "styled-components";
+
 export default function HistoryScreen() {
     return(
-        <div>HISTÓRICO DE HÁBITOS</div>
+        <Container>
+            <TopBar />
+            <h2>Histórico</h2>
+            <h4>Em breve você poderá ver o histórico dos seus hábitos aqui!</h4>
+            <BottomBar />
+        </Container>
     );
 }
+
+const Container = styled.div`
+    box-sizing: border-box;
+    margin: 70px 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100vh;
+    width: 100%;
+    background-color: #F2F2F2;
+
+    h2 {
+        font-family: 'Lexend Deca', sans-serif;
+        font-size: 23px;
+        color: #126BA5;
+    }
+
+    h4 {
+        font-family: 'Lexend Deca', sans-serif;
+        font-size: 18px;
+        color: #666666;
+    }
+`
